9d869f0378c64b942360346ed93dd6fb01ab502a
Jack <jack.70aw@gmail.com>
2026-08-06 15:32:01 +0330

    feat(core): add swagger doc and minor changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.enableShutdownHooks();
+  app.setGlobalPrefix('api');
   app.use(helmet());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   const corsOrigin = process.env.CORS_ORIGIN;
 
@@ -16,10 +28,20 @@ async function bootstrap() {
   }
 
   app.enableCors({
-    origin: corsOrigin.split(','),
+    origin: corsOrigin.split(',').map((origin) => origin.trim()),
     credentials: true,
   });
 
+  const config = new DocumentBuilder()
+    .setTitle('Crypto marketplace')
+    .setDescription('marketplace API documention')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
+  app.useLogger(['error', 'warn', 'log']);
   await app.listen(Number(process.env.PORT));
 }
 
